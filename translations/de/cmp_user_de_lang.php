893963173fa1b949befea1eb88c2daa711d15e25
inserv@winde-ganzig.de <mnisw1966gh>
2020-10-13 20:16:16 +0200

    Übersetzung hinzugefügt

--- translations/de/cmp_user_de_lang.php
+++ translations/de/cmp_user_de_lang.php
@@ -8,6 +8,7 @@ $sLangName = "Deutsch";
 
 $aLang = array(
     "charset"                                        => "UTF-8",
-    "CMP_USER_TEXT"                                  => "F&uuml;r Preise, bitte anmelden!"
-
+    "CMP_USER_TEXT"                                  => "F&uuml;r Preise, bitte anmelden!",
+    // Global
+    "WIDGET_PRODUCT_MOREINFO"                       =>  "Mehr Info",
 );
