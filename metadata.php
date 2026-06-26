84f3ca7ad3b1b134588d484c4544703dca7cb4ab
Steffen Winde <inserv@winde-ganzig.de>
2026-06-26 15:27:11 +0200

    Ändere Blocknamen von 'details_productmain_price' zu 'details_productmain_priceperunit' und passe die Template-Logik an

--- metadata.php
+++ metadata.php
@@ -36,7 +36,7 @@ $aModule = [
         [
             //Product Details
             'template'  => 'page/details/inc/productmain.tpl',
-            'block'     => 'details_productmain_price',
+            'block'     => 'details_productmain_priceperunit',
             'file'      => '/views/blocks/cmp_user_productmain.tpl'
         ],
         [
